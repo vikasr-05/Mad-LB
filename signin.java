e770952111b126c1aa07c28aa881f324b7251068
vikasvenki <93909839+vikasvenki@users.noreply.github.com>
2022-05-20 16:26:38 +0530

    Create signin.java

---
+++ signin.java
@@ -0,0 +1,45 @@
+package com.example.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.security.Key;
+
+public class Signin extends AppCompatActivity {
+    EditText UserName,password;
+    Button Signin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        UserName=(EditText)findViewById(R.id UserName);
+        password=(EditText) findViewById(R.id password);
+        Signin=(Button) findViewById(R.id Signin);
+
+        Bundle bundle=getIntent().getExtras();
+        String uname=bundle.getString(Key"Username");
+        String pwd=bundle.getString(Key"Password");
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String User=UserName.getText().toString();
+                String pass=password.getText().toString();
+                if(User.equals(uname)&&pass.equals(pwd))
+                {
+                    Toast.makeText(Signin.this, "success", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(Signin.this, "", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
