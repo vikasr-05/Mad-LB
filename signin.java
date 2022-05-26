58d6673b06445d1af6ad3ec2e0dcef7eab2b92cf
vikasvenki <93909839+vikasvenki@users.noreply.github.com>
2022-05-26 23:47:58 +0530

    Update signin.java

--- signin.java
+++ signin.java
@@ -1,45 +1,59 @@
-package com.example.signup;
+package com.example.prgm3_4mt20cs401;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import java.security.Key;
+public class MainActivity extends AppCompatActivity {
 
-public class Signin extends AppCompatActivity {
-    EditText UserName,password;
-    Button Signin;
+    EditText username,password;
+    Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_main);
 
-        UserName=(EditText)findViewById(R.id UserName);
-        password=(EditText) findViewById(R.id password);
-        Signin=(Button) findViewById(R.id Signin);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        signUpBtn = findViewById(R.id.signupbtn);
 
-        Bundle bundle=getIntent().getExtras();
-        String uname=bundle.getString(Key"Username");
-        String pwd=bundle.getString(Key"Password");
-        Signin.setOnClickListener(new View.OnClickListener() {
+        String regex = "^(?=.*[0-9])"
+                        +"(?=.*[a-z])(?=.*[A-Z])"
+                        +"(?=.*[@#$%^&+=])"
+                        + "(?=\\S+$).{8,20}$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String User=UserName.getText().toString();
+                String uname=username.getText().toString();
                 String pass=password.getText().toString();
-                if(User.equals(uname)&&pass.equals(pwd))
-                {
-                    Toast.makeText(Signin.this, "success", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Toast.makeText(Signin.this, "", Toast.LENGTH_SHORT).show();
+
+                Matcher matcher = pattern.matcher(pass);
+                if((uname.length()==0) || pass.length()==0) {
+                    Toast.makeText(getApplicationContext(),"usernmae or password cannot be empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    if(matcher.matches()) {
+                        Intent intent = new Intent(MainActivity.this, login.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("username", uname);
+                        bundle.putString("password", pass);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(),"Password pattern is not Proper", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
     }
+
 }
